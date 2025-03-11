be705d0687d1eb1b07cedfbcfdc1f72908fdaa93
Malav Patel <95128635+Malav2364@users.noreply.github.com>
2025-03-11 17:12:29 +0530

    build issues

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/j3CzBHBWoey0AbkrwX6DhnFW+nTY5WM+4qKTYR4qSI=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'standalone',
+    distDir: '.next',
+  };
+  
+  export default nextConfig;
